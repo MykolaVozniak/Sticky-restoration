23e504d9580ddc8d8acbcf5b2c0939d2ea3ebd40
MVozniak <mikola.v.2016@gmail.com>
2023-06-12 06:49:11 +0300

    first commit

---
+++ Sticky restoration/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Sticky_restoration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
